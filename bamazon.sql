edcd0e5b5e425c6ae37fa800ecf652681ae703bf
John Garrett <infantrygarrett@icloud.com>
2019-04-04 21:33:49 -0500

    Finished JS file

--- bamazon.sql
+++ bamazon.sql
@@ -5,10 +5,18 @@ CREATE DATABASE bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products(
-	item_id INT AUTO_INCREMENT,
+	item_id INTEGER(50) AUTO_INCREM /-ENT,
 	product_name VARCHAR(100) NOT NULL,
 	department_name VARCHAR(100) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
-	stock_quantity INT NOT NULL
+	stock_quantity INT NOT NULL,
 	PRIMARY KEY (item_id)
 );
+
+INSERT INTO products
+(product_name, department_name, price, stock_quantity)
+VALUES
+("Ipad","Electronics",649.99,6),
+
+    
+SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -40,3 +40,89 @@ function buyItems(){
         promptUser()
     })
 }
+// =======================================================================================================================
+
+function mainMenu(){
+    inquirier
+        .prompt([
+            {
+                name:"options",
+                message: "What would you like to do?",
+                type:"list",
+                choices:["Buy AN ITEM", "EXIT"]
+            }
+        ]).then(function(r){
+            if(r.options === "EXIT"){
+                console.log("GoodBye.")
+                connection.end();
+            }else{
+                buyOption();
+            }
+        })
+}
+
+
+function promptUser(){
+    inquirier
+        .prompt([
+            {
+                name: "itemID",
+                message: "Use the Item ID to select what you would like to buy.",
+                validate(value){
+                    return !isNaN(value) && value > 0
+                }
+            },
+            {
+                name: "amount",
+                message: "How many many do you want?",
+                validate(value){
+                    return !isNaN(value) && value > 0
+                }
+            }
+        ]).then(function(ans){
+            isAvaiable(ans.itemID, ans.amount)
+        })
+}
+
+function isAvaiable(id, amount){
+    connection.query(`
+    SELECT * FROM products WHERE ?`,
+    {
+        item_id: id
+    },
+    function(err, res){
+        if(err) throw err;
+        console.log(res)
+        if(res[0]["stock_quantity"] >= amount){
+            console.log("buying items " + amount);
+            fullfilOrder(id, amount)
+        }else{
+            console.log("Insufficent quantity");
+            mainMenu();
+        }
+    }
+
+    )
+}
+
+function fullfilOrder(id, amount){
+    connection.query(
+        `SELECT stock_quantity, price FROM products WHERE ?`, [{
+            item_id: id
+        }], function(err,res){
+            let price = res[0].price;
+            let currentAmount = res[0].stock_quantity;
+            currentAmount -= amount;
+        
+        connection.query(
+            ` UPDATE products SET ? WHERE ?`,[{stock_quantity: currentAmount}, {item_id: id}], function(err,res){
+                console.log("Thanks! Your total is $" + (parseInt(amount)*parseFloat(price) +"."));
+                mainMenu();
+            })
+        
+        }
+        )
+}
+
+//Running main program
+main();
